c61573db7da2bbfeaacdb29f52a7b264a87cb4fc
Ashraful-malik <ashrafulmalik71@gmail.com>
2021-08-28 18:57:18 +0530

    fixed token error

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -71,8 +71,9 @@ export default {
             iconUrl: locationImage,
           });
           L.marker([this.lat, this.lng], { icon: myIcon }).addTo(mymap);
+          const accessToken = process.env.VUE_APP_MAP_BOX_API_KEY;
           L.tileLayer(
-            "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+            `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${accessToken}`,
             {
               attribution:
                 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
